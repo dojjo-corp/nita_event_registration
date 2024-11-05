9a916a66bbdab8201549d41e9f7236b837d0e75d
Martinson @ NITA <hosuatetteh@gmail.com>
2024-11-05 16:58:27 +0000

    Event -ii

---
+++ src/js/index.js
@@ -0,0 +1,11 @@
+const nameElem = document.querySelector("input[name=fullname]");
+nameElem.style.color = "red";
+
+
+nameElem.addEventListener("input", (e) => {
+    // initialize get request
+
+    // send reauest with input element's value
+
+    // retrive
+})
